ad5a218ace65686f2acf0a18056fda899924f834
Ninex ConSciousX <84043851+Sarfaraaz2002@users.noreply.github.com>
2022-11-11 12:05:14 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Sleepy Kernel for Redmi Note 10 Pro & Pro Max by @ itsshashanksp
+kernel.string=The-OXYGEN
 do.devicecheck=1
 do.modules=0
 do.systemless=1
